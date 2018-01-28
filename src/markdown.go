6e6a980dd18d1de844ee729f88922eda7e555292
Honza Pokorny <me@honza.ca>
2018-01-28 19:48:55 -0400

    Move markdown bits to own file

---
+++ src/markdown.go
@@ -0,0 +1,69 @@
+package src
+
+import (
+	"fmt"
+	"io"
+	"sort"
+	"strings"
+)
+
+func SingleEmitMarkdown(w io.Writer, single Single) error {
+	fmt.Fprint(w, word_wrap(single.Content, MARKDOWN_WIDTH, "> "))
+	fmt.Fprint(w, "\n\n")
+	fmt.Fprint(w, FormatLocation(single.Location))
+	fmt.Fprint(w, single.Timestamp.String())
+	fmt.Fprint(w, "\n\n")
+
+	return nil
+}
+
+func AuthorEmitMarkdown(w io.Writer, author string, h NewAuthor) error {
+	fmt.Fprint(w, author)
+	fmt.Fprint(w, "\n")
+	fmt.Fprint(w, strings.Repeat("=", len(author)))
+	fmt.Fprint(w, "\n\n")
+
+	books := make([]string, 0, len(h))
+
+	for book := range h {
+		books = append(books, book)
+	}
+
+	sort.Strings(books)
+
+	for _, book := range books {
+		bookTitle := TruncateBookTitle(book)
+		fmt.Fprint(w, bookTitle)
+		fmt.Fprint(w, "\n")
+		fmt.Fprint(w, strings.Repeat("-", len(bookTitle)))
+		fmt.Fprint(w, "\n\n")
+
+		for _, single := range h[book] {
+			SingleEmitMarkdown(w, single)
+		}
+
+	}
+
+	return nil
+}
+
+func EmitMarkdown(w io.Writer, hs Highlights) error {
+	authors := make([]string, 0, len(hs))
+
+	for author := range hs {
+		authors = append(authors, author)
+	}
+
+	sort.Strings(authors)
+
+	for _, author := range authors {
+		err := AuthorEmitMarkdown(w, author, hs[author])
+		if err != nil {
+			return err
+		}
+		fmt.Fprint(w, "\n")
+
+	}
+
+	return nil
+}
--- src/parse.go
+++ src/parse.go
@@ -110,16 +110,6 @@ func FormatLocation(location Location) string {
 	return fmt.Sprintf("")
 }
 
-func SingleEmitMarkdown(w io.Writer, single Single) error {
-	fmt.Fprint(w, word_wrap(single.Content, MARKDOWN_WIDTH, "> "))
-	fmt.Fprint(w, "\n\n")
-	fmt.Fprint(w, FormatLocation(single.Location))
-	fmt.Fprint(w, single.Timestamp.String())
-	fmt.Fprint(w, "\n\n")
-
-	return nil
-}
-
 func TruncateBookTitle(s string) string {
 	if len(s) < MARKDOWN_WIDTH {
 		return s
@@ -128,57 +118,6 @@ func TruncateBookTitle(s string) string {
 	suffix := " [...]"
 	return s[0:MARKDOWN_WIDTH-len(suffix)] + suffix
 
-}
-
-func AuthorEmitMarkdown(w io.Writer, author string, h NewAuthor) error {
-	fmt.Fprint(w, author)
-	fmt.Fprint(w, "\n")
-	fmt.Fprint(w, strings.Repeat("=", len(author)))
-	fmt.Fprint(w, "\n\n")
-
-	books := make([]string, 0, len(h))
-
-	for book := range h {
-		books = append(books, book)
-	}
-
-	sort.Strings(books)
-
-	for _, book := range books {
-		bookTitle := TruncateBookTitle(book)
-		fmt.Fprint(w, bookTitle)
-		fmt.Fprint(w, "\n")
-		fmt.Fprint(w, strings.Repeat("-", len(bookTitle)))
-		fmt.Fprint(w, "\n\n")
-
-		for _, single := range h[book] {
-			SingleEmitMarkdown(w, single)
-		}
-
-	}
-
-	return nil
-}
-
-func EmitMarkdown(w io.Writer, hs Highlights) error {
-	authors := make([]string, 0, len(hs))
-
-	for author := range hs {
-		authors = append(authors, author)
-	}
-
-	sort.Strings(authors)
-
-	for _, author := range authors {
-		err := AuthorEmitMarkdown(w, author, hs[author])
-		if err != nil {
-			return err
-		}
-		fmt.Fprint(w, "\n")
-
-	}
-
-	return nil
 }
 
 func ValidateOutputFormat(output string) error {
